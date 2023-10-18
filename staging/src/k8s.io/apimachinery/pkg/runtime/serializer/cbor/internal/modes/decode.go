3eab41db8e59420da7d611aca05c8336e00c6afe
Ben Luddy <bluddy@redhat.com>
2023-10-18 13:36:13 -0400

    WIP: Add CBOR Serializer implementation.
    Co-authored-by: Suriyan Subbarayan <suriyansub710@gmail.com>

---
+++ staging/src/k8s.io/apimachinery/pkg/runtime/serializer/cbor/internal/modes/decode.go
@@ -0,0 +1,88 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package modes
+
+import (
+	"reflect"
+
+	"github.com/fxamacker/cbor/v2"
+)
+
+var Decode cbor.DecMode = func() cbor.DecMode {
+	decode, err := cbor.DecOptions{
+		// Maps with duplicate keys are well-formed but invalid according to the CBOR spec
+		// and never acceptable. Unlike the JSON serializer, inputs containing duplicate map
+		// keys are rejected outright and not surfaced as a strict decoding error.
+		DupMapKey: cbor.DupMapKeyEnforcedAPF,
+
+		// For JSON parity, decoding an RFC3339 string into time.Time needs to be accepted
+		// with or without tagging. If a tag number is present, it must be valid.
+		TimeTag: cbor.DecTagOptional,
+
+		// Observed depth up to 16 in fuzzed batch/v1 CronJobList. JSON implementation limit
+		// is 10000.
+		MaxNestedLevels: 64,
+
+		MaxArrayElements: 1024,
+		MaxMapPairs:      1024,
+
+		// Indefinite-length sequences aren't produced by this serializer, but other
+		// implementations can.
+		IndefLength: cbor.IndefLengthAllowed,
+
+		// Accept inputs that contain CBOR tags.
+		TagsMd: cbor.TagsAllowed,
+
+		// Decode type 0 (unsigned integer) as int64.
+		// TODO: IntDecConvertSigned errors on overflow, JSON will try to fall back to float64.
+		IntDec: cbor.IntDecConvertSigned,
+
+		// Disable producing map[cbor.ByteString]interface{}, which is not acceptable for
+		// decodes into interface{}.
+		MapKeyByteString: cbor.MapKeyByteStringForbidden,
+
+		// Error on map keys that don't map to a field in the destination struct.
+		ExtraReturnErrors: cbor.ExtraDecErrorUnknownField,
+
+		// Decode maps into concrete type map[string]interface{} when the destination is an
+		// interface{}.
+		DefaultMapType: reflect.TypeOf(map[string]interface{}(nil)),
+
+		// A CBOR text string whose content is not a valid UTF-8 sequence is well-formed but
+		// invalid according to the CBOR spec. Reject invalid inputs. Encoders are
+		// responsible for ensuring that all text strings they produce contain valid UTF-8
+		// sequences and may use the byte string major type to encode strings that have not
+		// been validated.
+		UTF8: cbor.UTF8RejectInvalid,
+	}.DecMode()
+	if err != nil {
+		panic(err)
+	}
+	return decode
+}()
+
+// DecodeLax is derived from Decode, but does not complain about unknown fields in the input.
+var DecodeLax cbor.DecMode = func() cbor.DecMode {
+	opts := Decode.DecOptions()
+	opts.ExtraReturnErrors &^= cbor.ExtraDecErrorUnknownField
+	opts.DefaultMapType = reflect.TypeOf(map[string]interface{}(nil)) // DefaultMapType isn't set properly by DecOptions, set it again here until the bugfix is adopted.
+	dm, err := opts.DecMode()
+	if err != nil {
+		panic(err)
+	}
+	return dm
+}()
---
+++ staging/src/k8s.io/apimachinery/pkg/runtime/serializer/cbor/internal/modes/roundtrip_test.go
@@ -0,0 +1,208 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package modes_test
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/fxamacker/cbor/v2"
+)
+
+func nilPointerFor[T interface{}]() *T {
+	return nil
+}
+
+func TestRoundtrip(t *testing.T) {
+	type modePair struct {
+		enc cbor.EncMode
+		dec cbor.DecMode
+	}
+
+	for _, tc := range []struct {
+		name      string
+		modePairs []modePair
+		obj       interface{}
+	}{
+		{
+			name: "nil slice",
+			obj:  []interface{}(nil),
+		},
+		{
+			name: "nil map",
+			obj:  map[string]interface{}(nil),
+		},
+		{
+			name: "empty slice",
+			obj:  []interface{}{},
+		},
+		{
+			name: "empty map",
+			obj:  map[string]interface{}{},
+		},
+		{
+			name: "nil pointer to slice",
+			obj:  nilPointerFor[[]interface{}](),
+		},
+		{
+			name: "nil pointer to map",
+			obj:  nilPointerFor[map[string]interface{}](),
+		},
+		{
+			name: "nonempty string",
+			obj:  "hello world",
+		},
+		{
+			name: "empty string",
+			obj:  "",
+		},
+		{
+			name: "true",
+			obj:  true,
+		},
+		{
+			name: "false",
+			obj:  false,
+		},
+		{
+			name: "int64",
+			obj:  int64(5),
+		},
+		{
+			name: "int64 max",
+			obj:  math.MaxInt64,
+		},
+		{
+			name: "int64 min",
+			obj:  math.MinInt64,
+		},
+		{
+			name: "float64",
+			obj:  float64(2.71),
+		},
+		{
+			name: "float64 max",
+			obj:  math.MaxFloat64,
+		},
+		{
+			name: "float64 no fractional component",
+			obj:  float64(5),
+		},
+		{
+			name: "time.Time",
+			obj:  time.Date(2222, time.May, 4, 12, 13, 14, 123, time.UTC),
+		},
+		{
+			name: "int64 omitempty",
+			obj: struct {
+				V int64 `json:"v,omitempty"`
+			}{},
+		},
+		{
+			name: "float64 omitempty",
+			obj: struct {
+				V float64 `json:"v,omitempty"`
+			}{},
+		},
+		{
+			name: "string omitempty",
+			obj: struct {
+				V string `json:"v,omitempty"`
+			}{},
+		},
+		{
+			name: "bool omitempty",
+			obj: struct {
+				V bool `json:"v,omitempty"`
+			}{},
+		},
+		{
+			name: "nil pointer omitempty",
+			obj: struct {
+				V *struct{} `json:"v,omitempty"`
+			}{},
+		},
+		{
+			name: "nil pointer to slice as struct field",
+			obj: struct {
+				V *[]interface{} `json:"v"`
+			}{},
+		},
+		{
+			name: "nil pointer to slice as struct field with omitempty",
+			obj: struct {
+				V *[]interface{} `json:"v,omitempty"`
+			}{},
+		},
+		{
+			name: "nil pointer to map as struct field",
+			obj: struct {
+				V *map[string]interface{} `json:"v"`
+			}{},
+		},
+		{
+			name: "nil pointer to map as struct field with omitempty",
+			obj: struct {
+				V *map[string]interface{} `json:"v,omitempty"`
+			}{},
+		},
+	} {
+		mps := tc.modePairs
+		if len(mps) == 0 {
+			// Default is all modes to all modes.
+			mps = []modePair{}
+			for _, em := range allEncModes {
+				for _, dm := range allDecModes {
+					mps = append(mps, modePair{enc: em, dec: dm})
+				}
+			}
+		}
+
+		for _, mp := range mps {
+			encModeName, ok := encModeNames[mp.enc]
+			if !ok {
+				t.Fatal("test case configured to run against unrecognized encode mode")
+			}
+
+			decModeName, ok := decModeNames[mp.dec]
+			if !ok {
+				t.Fatal("test case configured to run against unrecognized decode mode")
+			}
+
+			t.Run(fmt.Sprintf("enc=%s/dec=%s/%s", encModeName, decModeName, tc.name), func(t *testing.T) {
+				original := tc.obj
+
+				b, err := mp.enc.Marshal(original)
+				if err != nil {
+					t.Fatalf("unexpected error from Marshal: %v", err)
+				}
+
+				final := reflect.New(reflect.TypeOf(original))
+				err = mp.dec.Unmarshal(b, final.Interface())
+				if err != nil {
+					t.Fatalf("unexpected error from Unmarshal: %v", err)
+				}
+				if !reflect.DeepEqual(original, final.Elem().Interface()) {
+					t.Errorf("roundtrip difference:\nwant: %#v\ngot: %#v", original, final)
+				}
+			})
+		}
+	}
+}
